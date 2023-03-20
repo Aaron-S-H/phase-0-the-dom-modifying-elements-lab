f548bdd7c6ab67822ddc5a6027f5c9a3aa769cdd
Aaron Hemphill <hemphill.aaron@gmail.com>
2023-03-20 12:19:06 +0100

    commit tree 19803d33011ed22a0a827fafc27146626cdd5b5b

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+const main = document.getElementById('main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.innerHTML = "Aaron is the champion";
+
+const body = document.querySelector('body');
+
+document.body.append(newHeader);
